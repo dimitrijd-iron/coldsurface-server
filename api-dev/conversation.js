b33a6a6f19a484520cbcf18d9653f65472957111
dimitrijd <dimitrijdugan@gmail.com>
2021-03-03 20:33:01 +0100

    first commit

--- api-dev/conversation.js
+++ api-dev/conversation.js
@@ -1,12 +1,13 @@
 const { WebClient, LogLevel } = require("@slack/web-api");
+
 const client = new WebClient(process.env.SLACK_TOKEN, {
-  logLevel: LogLevel.DEBUG,
+//  logLevel: LogLevel.DEBUG,
 });
 
 (async () => {
   try {
     const result = await client.conversations.list();
-    console.log(result.channels);
+    console.log(JSON.stringify(result.channels));
   } catch (error) {
     console.log(error);
   }
