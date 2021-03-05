71428a5d260f99dcb1613a8ab7d06d8ce45d6032
dimitrijd <dimitrijdugan@gmail.com>
2021-03-05 17:26:00 +0100

    first step datapipeline

---
+++ datapipeline/datapipeline.js
@@ -0,0 +1,12 @@
+const slack = require("./services/slack.service");
+const watson = require("./services/watson.service");
+
+// read-data from slack
+
+// clean slack data
+// read objectID from mongo
+// filter-out duplicates
+
+// augment the data with watson
+
+// push the data to mongo
