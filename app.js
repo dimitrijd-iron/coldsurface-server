096e5aa37ecb4d56ff2c4bb96f7664757fb4caa4
dimitrijd <dimitrijdugan@gmail.com>
2021-03-08 16:36:59 +0100

    pre-deployment

--- app.js
+++ app.js
@@ -60,10 +60,13 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // ROUTER MIDDLEWARE
 app.use("/auth", authRouter);
-
 app.use("/api", dataRouter);
-// app.use("/api/user", usersRouter);
-// app.use("/api/projects", projectsRouter);
+
+// ROUTE FOR SERVING REACT APP (index.html)
+app.use((req, res, next) => {
+  // If no previous routes match the request, send back the React app.
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // ERROR HANDLING
 //  Catch 404 and respond with error message
